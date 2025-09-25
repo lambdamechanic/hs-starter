3e26a6e0119e7e5a7085a37cb5e26ff7e0ddb729
Mark Wotton <mwotton@gmail.com>
2025-09-26 04:53:07 +0700

    chore: enable precompressed frontend output

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -7,7 +7,8 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html'
+      fallback: 'index.html',
+      precompress: true
     }),
     alias: {
       $lib: 'src/lib'
